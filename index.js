b2288b1ceb0adf728829116fdee9f70bcbb38045
Gamers_indo1223 <76719536+gamersindo1223@users.noreply.github.com>
2023-07-02 14:53:05 +0000

    Now using the official api

--- index.js
+++ index.js
@@ -1,105 +1,54 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
 const chromium = require("@sparticuz/chromium");
-const puppeteer = require("puppeteer-core");
 const cors = require("cors");
 const express = require("express");
-const rateLimit = require('express-rate-limit')
-const cheerio = require("cheerio");
+const puppeteer = require("puppeteer-core");
+const rateLimit = require('express-rate-limit');
+
 const app = express();
 const port = process.env.PORT ?? 8080;
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 const createAccountLimiter = rateLimit({
 	windowMs: 60 * 60 * 1000, // 1 hour
-	max: 5, // Limit each IP to 5 create account requests per `window` (here, per hour)
+	max: 6, // Limit each IP to 5 create account requests per `window` (here, per hour)
 	message:
 		'Too many request created from this IP, please try again after an hour',
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 app.use(createAccountLimiter)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/lahelu/random", async (req, res) => {
-  try {
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(),
-      headless: chromium.headless,
-    });
-    chromium.setHeadlessMode = true;
-  
-    // Optional: If you'd like to disable webgl, true is the default.
-    chromium.setGraphicsMode = false;
-    const page = await browser.newPage();
-    async function getShuffle() {
-      await page.setUserAgent(
-        "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
-      );
-      const response = await page.goto(`https://lahelu.com/shuffle`, {
-      });
-      await page.setRequestInterception(true);
-      const rejectRequestPattern = [
-        "googlesyndication.com",
-        "/*.doubleclick.net",
-        "/*.amazon-adsystem.com",
-        "/*.adnxs.com",
-      ];
-      const blockList = [];
-      page.on("request", (request) => {
-        if (
-          rejectRequestPattern.find((pattern) => request.url().match(pattern))
-        ) {
-          blockList.push(request.url());
-          request.abort();
-        } else request.continue();
-      });
-      if (response.status() !== 200)
-        return res.json({
-          error: true,
-          statuscode: response.status(),
-          message: "website is down",
-        });
-      await page.waitForSelector("article");
-      const $ = cheerio.load(await page.content());
-      const currpage = $('div[class^="Div_space__7HOAc Div_gap-xs__yTwgF"]');
-      //get author thing
-      const authorpfp = $("article").find("img").attr("src");
-      const authorname = $('span[class^="AuthorMeta_username__yRXGw"]').text();
-      //get media thing
-      const title = currpage.find("h1").text();
-      const media =
-        currpage.find("video").attr("src") || currpage.find("img").attr("src");
-      const category = $(
-        'div[class^="Div_space__7HOAc Div_gap-2xs__OnWXN Div_wrap__Ae_Kw"]'
-      ).text();
-      if (category.toLowerCase().includes("sensitif")) {
-        await browser.close();
-        return await getShuffle();
-      }
-      console.log("should returns something");
-      return {
-        author: {
-          name: authorname,
-          profilepicture: authorpfp,
-        },
-        title: title,
-        media: media,
-        category: category,
-      };
-    }
-    const a = await getShuffle();
-    res.json({
-      error: false,
-      data: a,
-    });
-    await browser.close();
-  } catch (e) {
-    console.log(e);
+  const listofcategories = require("./categories.json")
+  const response = new Object()
+  const categorylist = []
+  const data = (await axios.get("https://lahelu.com/api/post/get-shuffle")).data.postInfo
+  for (key in data.categories){
+    categorylist.push(listofcategories[data.categories[key]])
   }
+  response["postID"] = `https://lahelu.com/post/${data.postID}`
+  response["title"] = data.title
+  response["totalUpvotes"] = data.totalUpvotes
+  response["totalDownvotes"] = data.totalDownvotes
+  response["totalComments"] = data.totalComments
+  response["createTime"] = data.createTime
+  response["media"] = `https://cache.lahelu.com/${data.media}`
+  response["mediaThumbnail"] = `https://cache.lahelu.com/${data.mediaThumbnail}`
+  response["sensitive"] = data.sensitive
+  response["categories"] = categorylist
+  response["userUsername"] = data.userUsername
+  response["userAvatar"] = `https://cache.lahelu.com/${data.userAvatar}`
+  res.json({
+    error: false,
+    data: response
+  })
 });
 
 app.get("/lahelu/user/:username", async (req, res) => {
